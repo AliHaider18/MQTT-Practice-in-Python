a4a37ae494086debfa1175396856edfcf05840d7
Ali Haider Randhawa <93574635+AliHaider18@users.noreply.github.com>
2023-03-27 18:59:04 +0500

    Update mqtt_subscribe.py

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
 
+#Decoding the message
 def on_message(client, userdata, message):
     print("Received message: ", str(message.payload.decode("utf-8")))
 
@@ -9,6 +10,7 @@ client = mqtt.Client("Smartphone")
 client.connect(mqttBroker)
 
 client.loop_start()
+#Subscribe to Temperature 
 client.subscribe("TEMPERATURE")
 client.on_message = on_message
 time.sleep(30)
